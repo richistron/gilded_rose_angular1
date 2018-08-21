75ca04f04e07daebd6b33f9fa95ae5a625be0c7f
Ricardo Rivas <richistron@gmail.com>
2018-08-20 22:33:35 -0500

    adds service-worker.js

--- webpack.config.es6.js
+++ webpack.config.es6.js
@@ -1,9 +1,11 @@
+import webpack from 'webpack';
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 const conf = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -15,8 +17,14 @@ const conf = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
+    new HtmlWebpackPlugin({
+      serviceWorker: '/service-worker.js',
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {from: './public/service-worker.js', to: './service-worker.js'}
+    ]),
+  ],
 };
 
 export default conf;
